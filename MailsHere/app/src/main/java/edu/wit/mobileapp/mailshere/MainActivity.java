98a732b49f0318a4f2a7fc26a70be482af1cc994
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-07-23 23:34:21 -0400

    basic home page design

---
+++ MailsHere/app/src/main/java/edu/wit/mobileapp/mailshere/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.wit.mobileapp.mailshere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button open = (Button)findViewById(R.id.open_up);
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent().setClass(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
